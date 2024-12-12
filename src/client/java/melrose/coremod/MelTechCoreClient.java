9db58c32a95a3e356fe4fa9cee8f21d268af62d1
Midford <Killer_Mid@outlook.com>
2024-12-12 15:04:40 +0300

    Initial commit

---
+++ src/client/java/melrose/coremod/MelTechCoreClient.java
@@ -0,0 +1,10 @@
+package melrose.coremod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MelTechCoreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
